2a1d8b9ce2ca54ad3017b60da8095019cd8492fc
MarcosGomezVega <mgv1017@alu.ubu.es>
2025-06-24 13:07:48 +0200

    Move Type codes from Movie to Price

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -12,10 +12,6 @@ package ubu.gii.dass.refactoring;
  */
 
 public class Movie {
-	public static final int CHILDRENS = 2;
-	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
-
 	private String _title;
 	Price _price;
 
@@ -27,13 +23,13 @@ public class Movie {
 	
 	public void setPriceCode(int arg) {
 		switch(arg) {
-			case Movie.REGULAR:
+			case Price.REGULAR:
 				_price = new Regular();
 				break;
-			case Movie.NEW_RELEASE:
+			case Price.NEW_RELEASE:
 				_price = new NewRelease();
 				break;
-			case Movie.CHILDRENS:
+			case Price.CHILDRENS:
 				_price = new Children();
 				break;
 			default:
--- src/ubu/gii/dass/refactoring/NewRelease.java
+++ src/ubu/gii/dass/refactoring/NewRelease.java
@@ -4,7 +4,7 @@ package ubu.gii.dass.refactoring;
 public class NewRelease extends Price {
 
 	int getPriceCode() {
-		return Movie.NEW_RELEASE;
+		return Price.NEW_RELEASE;
 	}
 	public int getFrecuentRenterPoints(Rental rental) {
         if (rental.getDaysRented() > 1) return 2;
